ac5ac3578565096ebd95c07af2c6e516263efe15
Phil Pennock <pdp@spodhuis.org>
2015-09-17 23:38:17 -0400

    Skeletal Unicode loading, go generate
    Using crude shell to make the file which embeds the raw Unicode data in
    a `.go` file.  For now, storing the data as one big contiguous `[]byte`
    blob.  It's largish, but still less than 2MB and would be loaded and
    parsed in a scripting language anyway, so overhead minimal.
    
    Planning to be inefficient with parsing/referencing for the time being.
    If I don't mess up the interfaces too much, should be able to optimize
    later.

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+//go:generate ./util/update_unicode
+
 import (
 	"os"
 
--- unicode/unicode.go
+++ unicode/unicode.go
@@ -1,7 +1,13 @@
 package unicode
 
+import (
+	"fmt"
+	"os"
+)
+
 type Unicode struct{}
 
 func Load() Unicode {
+	fmt.Fprintf(os.Stderr, "have %d bytes of raw data\n", len(rawData))
 	return Unicode{}
 }
