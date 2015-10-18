028b262c63d7b610496ef94c37b0684758a55ee3
Phil Pennock <pdp@spodhuis.org>
2015-10-18 07:53:32 -0400

    Add basic table tests
    NB: something is wrong with column alignment.

---
+++ table/table_test.go
@@ -0,0 +1,76 @@
+// Copyright © 2015 Phil Pennock.
+// All rights reserved, except as granted under license.
+// Licensed per file LICENSE.txt
+
+package table
+
+import (
+	"testing"
+
+	"github.com/liquidgecka/testlib"
+)
+
+// should this instead be in a sub-package importing the parent, and using
+// Example functions with output in comments?
+func TestBasicTables(t *testing.T) {
+	T := testlib.NewT(t)
+	T.Equal(Supported(), true, "table should be supported")
+
+	// beware https://github.com/apcera/termtables/issues/28 and problems rendering twice if have headers.
+
+	tb := New()
+	T.Equal(tb.Empty(), true, "new table should be empty")
+
+	should := "" +
+		"╭╮\n" +
+		"╰╯\n" +
+		""
+	T.Equal(tb.Render(), should, "empty table renders XXX")
+
+	tb = New()
+	should = "" +
+		"╭────────┬────────────┬──────╮\n" +
+		"│ foo    │ loquacious │ x    │\n" +
+		"├────────┼────────────┼──────┤\n" +
+		"│ 42     │ .          │ fred │\n" +
+		"│ snerty │ word       │ r    │\n" +
+		"╰────────┴────────────┴──────╯\n" +
+		""
+	tb.AddHeaders("foo", "loquacious", "x")
+	tb.AddRow(42, ".", "fred")
+	tb.AddRow("snerty", "word", "r")
+	T.Equal(tb.Render(), should, "basic table should render cleanly")
+
+	tb = New()
+	should = "" +
+		"╭────────┬────────────┬──────────╮\n" +
+		"│ foo    │ loquacious │ x        │\n" +
+		"├────────┼────────────┼──────────┤\n" +
+		"│ 42     │ .          │ frederic │\n" +
+		"├────────┼────────────┼──────────┤\n" +
+		"│ snerty │ word       │ 3        │\n" +
+		"╰────────┴────────────┴──────────╯\n" +
+		""
+	tb.AddHeaders("foo", "loquacious", "x")
+	tb.AddRow(42, ".", "frederic")
+	tb.AddSeparator()
+	tb.AddRow("snerty", "word", 3)
+	T.Equal(tb.Render(), should, "basic table with separator added")
+
+	// FIXME:
+	// 1. make alignment actually work
+	tb = New()
+	should = "" +
+		"╭────────┬────────────┬──────────╮\n" +
+		"│ foo    │ loquacious │ x        │\n" +
+		"├────────┼────────────┼──────────┤\n" +
+		"│ 42     │ .          │ frederic │\n" +
+		"│ snerty │ word       │ 3        │\n" + // FIXME: this is wrong
+		"╰────────┴────────────┴──────────╯\n" +
+		""
+	tb.AddHeaders("foo", "loquacious", "x")
+	tb.AddRow(42, ".", "frederic")
+	tb.AddRow("snerty", "word", 3)
+	tb.AlignColumn(3, RIGHT)
+	T.Equal(tb.Render(), should, "basic table with 3rd column right-aligned")
+}
