philpennock/character

commit 8b30966c66f0fea714c2d7ba2bdf130d8056564c

Move table-rendering flags into resultset pkg
Support the set of --verbose/--net-verbose(/internal-stuff) more
uniformly by making it an accessor function.

Move name/named/code across and then add browse which gains the
--net-verbose as a result.

commands/named/named.go
@@ -6,7 +6,6 @@ package named
 
 import (
 	"errors"
-	"fmt"
 	"strings"
 
 	"github.com/atotto/clipboard"
@@ -20,15 +19,12 @@ import (
 )
 
 var flags struct {
-	clipboard     bool
-	internalDebug bool
-	join          bool
-	livevim       bool
-	netVerbose    bool
-	oneline       bool
-	search        bool
-	unsorted      bool
-	verbose       bool
+	clipboard bool
+	join      bool
+	livevim   bool
+	oneline   bool
+	search    bool
+	unsorted  bool
 }
 
 // FIXME: make dedicated type, embed search info
@@ -43,11 +39,15 @@ var namedCmd = &cobra.Command{
 	Use:   "named [name of character]",
 	Short: "shows character with given name",
 	Run: func(cmd *cobra.Command, args []string) {
+		if err := resultset.FlagsOkay(); err != nil {
+			root.Errorf("%s", err)
+			return
+		}
 		srcs := sources.NewFast()
 		if flags.search {
 			srcs.LoadUnicodeSearch()
 		}
-		if flags.verbose && flags.livevim {
+		if flags.livevim {
 			srcs.LoadLiveVim()
 		}
 		results := resultset.New(srcs, len(args))
@@ -107,35 +107,18 @@ var namedCmd = &cobra.Command{
 			}
 		}
 
-		if flags.verbose {
-			results.PrintTables()
-		} else if flags.netVerbose {
-			results.SelectFieldsNet()
-			results.PrintTables()
-		} else if flags.internalDebug {
-			results.SelectFieldsDebug()
-			results.PrintTables()
-		} else if flags.oneline {
-			fmt.Println(results.StringPlain(resultset.PRINT_RUNE))
-		} else {
-			results.PrintPlain(resultset.PRINT_RUNE)
-		}
+		results.RenderPerCmdline(resultset.PRINT_RUNE)
 	},
 }
 
 func init() {
 	namedCmd.Flags().BoolVarP(&flags.clipboard, "clipboard", "c", false, "copy resulting chars to clipboard too")
-	namedCmd.Flags().BoolVarP(&flags.internalDebug, "internal-debug", "", false, "")
-	namedCmd.Flags().MarkHidden("internal-debug")
 	namedCmd.Flags().BoolVarP(&flags.join, "join", "j", false, "all args are for one char name")
 	namedCmd.Flags().BoolVarP(&flags.livevim, "livevim", "l", false, "load full vim data (for verbose)")
 	namedCmd.Flags().BoolVarP(&flags.oneline, "oneline", "1", false, "multiple chars on one line")
 	namedCmd.Flags().BoolVarP(&flags.search, "search", "/", false, "search for words, not full name")
 	namedCmd.Flags().BoolVarP(&flags.unsorted, "unsorted", "u", false, "do not sort search results")
-	if resultset.CanTable() {
-		namedCmd.Flags().BoolVarP(&flags.verbose, "verbose", "v", false, "show information about the character")
-		namedCmd.Flags().BoolVarP(&flags.netVerbose, "net-verbose", "N", false, "show net-biased information (punycode, etc)")
-	}
+	resultset.RegisterCmdFlags(namedCmd) // verbose v | net-verbose N | internal-debug
 	if clipboard.Unsupported {
 		// We don't want to only register the flag if clipboard is supported,
 		// because that makes client portability more problematic.  Instead, we

commands/browse/browse.go
@@ -26,7 +26,6 @@ var flags struct {
 	livevim    bool
 	startrune  int
 	stoprune   int
-	verbose    bool
 }
 
 var browseCmd = &cobra.Command{
@@ -110,12 +109,12 @@ var browseCmd = &cobra.Command{
 			return
 		}
 
-		results.PrintTables()
+		results.RenderPerCmdline(resultset.PRINT_NAME)
 	},
 }
 
 func showBlocks(srcs *sources.Sources) {
-	if !flags.verbose {
+	if !resultset.CmdVerbose() {
 		for _, blockInfo := range srcs.UBlocks.ListBlocks() {
 			fmt.Printf("%s\n", blockInfo.Name)
 		}
@@ -151,6 +150,6 @@ func init() {
 	browseCmd.Flags().BoolVarP(&flags.livevim, "livevim", "l", false, "load full vim data")
 	browseCmd.Flags().IntVarP(&flags.startrune, "from", "f", 0, "show range starting at this value")
 	browseCmd.Flags().IntVarP(&flags.stoprune, "to", "t", 0, "show range ending at this value")
-	browseCmd.Flags().BoolVarP(&flags.verbose, "verbose", "v", false, "show information about block-names (for list-blocks)")
+	resultset.RegisterCmdFlags(browseCmd) // verbose v | net-verbose N | internal-debug
 	root.AddCommand(browseCmd)
 }

commands/code/code.go
@@ -23,13 +23,10 @@ import (
 )
 
 var flags struct {
-	base          intconvBase
-	clipboard     bool
-	internalDebug bool
-	livevim       bool
-	netVerbose    bool
-	utf8hex       bool
-	verbose       bool
+	base      intconvBase
+	clipboard bool
+	livevim   bool
+	utf8hex   bool
 }
 
 // FIXME: make dedicated type, embed search info
@@ -47,8 +44,13 @@ var codeCmd = &cobra.Command{
 	Use:   "code [codepoint ...]",
 	Short: "shows character with codepoint",
 	Run: func(cmd *cobra.Command, args []string) {
+		if err := resultset.FlagsOkay(); err != nil {
+			root.Errorf("%s", err)
+			return
+		}
+
 		srcs := sources.NewFast()
-		if flags.verbose && flags.livevim {
+		if flags.livevim {
 			srcs.LoadLiveVim()
 		}
 
@@ -149,17 +151,7 @@ var codeCmd = &cobra.Command{
 			}
 		}
 
-		if flags.verbose {
-			results.PrintTables()
-		} else if flags.netVerbose {
-			results.SelectFieldsNet()
-			results.PrintTables()
-		} else if flags.internalDebug {
-			results.SelectFieldsDebug()
-			results.PrintTables()
-		} else {
-			results.PrintPlain(resultset.PRINT_RUNE)
-		}
+		results.RenderPerCmdline(resultset.PRINT_RUNE)
 	},
 }
 
@@ -168,12 +160,7 @@ func init() {
 	codeCmd.Flags().BoolVarP(&flags.livevim, "livevim", "l", false, "load full vim data (for verbose)")
 	codeCmd.Flags().BoolVarP(&flags.utf8hex, "utf8hex", "H", false, "take UTF-8 Hex-encoded codepoint")
 	codeCmd.Flags().VarP(&flags.base, "base", "b", "numeric base for code-ponts [default: usual parse rules]")
-	if resultset.CanTable() {
-		codeCmd.Flags().BoolVarP(&flags.verbose, "verbose", "v", false, "show information about the character")
-		codeCmd.Flags().BoolVarP(&flags.netVerbose, "net-verbose", "N", false, "show net-biased information (punycode, etc)")
-		codeCmd.Flags().BoolVarP(&flags.internalDebug, "internal-debug", "", false, "")
-		codeCmd.Flags().MarkHidden("internal-debug")
-	}
+	resultset.RegisterCmdFlags(codeCmd) // verbose v | net-verbose N | internal-debug
 	if clipboard.Unsupported {
 		// We don't want to only register the flag if clipboard is supported,
 		// because that makes client portability more problematic.  Instead, we

commands/name/name.go
@@ -22,19 +22,21 @@ import (
 
 var flags struct {
 	encoding      string
-	internalDebug bool
 	listEncodings bool
 	livevim       bool
-	netVerbose    bool
 	punyIn        bool
 	hexInput      bool
-	verbose       bool
 }
 
 var nameCmd = &cobra.Command{
 	Use:   "name [char... [char...]]",
 	Short: "shows information about supplied characters",
 	Run: func(cmd *cobra.Command, args []string) {
+		if err := resultset.FlagsOkay(); err != nil {
+			root.Errorf("%s", err)
+			return
+		}
+
 		if flags.listEncodings {
 			// This should be deprecated, now that we have `known` top-level command
 			cmd.Printf("%s %s: these names (and some aliases) are known:\n", root.Cobra().Name(), cmd.Name())
@@ -51,7 +53,7 @@ var nameCmd = &cobra.Command{
 		}
 
 		srcs := sources.NewFast()
-		if flags.verbose && flags.livevim {
+		if flags.livevim {
 			srcs.LoadLiveVim()
 		}
 		approxCharCount := 0
@@ -92,17 +94,7 @@ var nameCmd = &cobra.Command{
 			}
 		}
 
-		if flags.verbose {
-			results.PrintTables()
-		} else if flags.netVerbose {
-			results.SelectFieldsNet()
-			results.PrintTables()
-		} else if flags.internalDebug {
-			results.SelectFieldsDebug()
-			results.PrintTables()
-		} else {
-			results.PrintPlain(resultset.PRINT_NAME)
-		}
+		results.RenderPerCmdline(resultset.PRINT_NAME)
 	},
 }
 
@@ -111,12 +103,9 @@ func init() {
 	nameCmd.Flags().BoolVarP(&flags.hexInput, "hex-input", "H", false, "take Hex-encoded input")
 	nameCmd.Flags().BoolVarP(&flags.listEncodings, "list-encodings", "", false, "list -e encodings & exit")
 	nameCmd.Flags().BoolVarP(&flags.punyIn, "punycode-input", "p", false, "decode punycode on cmdline")
+	resultset.RegisterCmdFlags(nameCmd) // verbose v | net-verbose N | internal-debug
 	if resultset.CanTable() {
-		nameCmd.Flags().BoolVarP(&flags.internalDebug, "internal-debug", "", false, "")
-		nameCmd.Flags().MarkHidden("internal-debug")
 		nameCmd.Flags().BoolVarP(&flags.livevim, "livevim", "l", false, "load full vim data (for verbose)")
-		nameCmd.Flags().BoolVarP(&flags.netVerbose, "net-verbose", "N", false, "show net-biased information (punycode, etc)")
-		nameCmd.Flags().BoolVarP(&flags.verbose, "verbose", "v", false, "show information about the character")
 	}
 	// FIXME: support verbose results without tables
 

resultset/cmdrender.go
@@ -1,0 +1,74 @@
+// Copyright © 2017 Phil Pennock.
+// All rights reserved, except as granted under license.
+// Licensed per file LICENSE.txt
+
+package resultset
+
+import (
+	"errors"
+
+	"github.com/spf13/cobra"
+)
+
+// Rather than repeat verbose/net-verbose/etc in each command, have flags here
+
+var resultCmdFlags struct {
+	internalDebug bool
+	netVerbose    bool
+	verbose       bool
+}
+
+// RegisterCmdFlags adds the --verbose/--net-verbose/--internal-debug flags to a Cobra cmd.
+func RegisterCmdFlags(cmd *cobra.Command) {
+	if !CanTable() {
+		return
+	}
+	cmd.Flags().BoolVarP(&resultCmdFlags.internalDebug, "internal-debug", "", false, "")
+	cmd.Flags().MarkHidden("internal-debug")
+	cmd.Flags().BoolVarP(&resultCmdFlags.netVerbose, "net-verbose", "N", false, "show net-biased information (punycode, etc)")
+	cmd.Flags().BoolVarP(&resultCmdFlags.verbose, "verbose", "v", false, "show information about the character")
+}
+
+// CmdVerbose indicates if a documented verbose flag was set; occasionally commands want to know
+// for reasons other than a resultset (eg, 'character browse -vB').
+func CmdVerbose() bool {
+	return resultCmdFlags.netVerbose || resultCmdFlags.verbose
+}
+
+// ErrIncompatibleFlags indicates that multiple types of verboseness were simultaneously requested.
+var ErrIncompatibleFlags = errors.New("incompatible table-rendering flags")
+
+// FlagsOkay returns either ErrIncompatibleFlags or nil
+func FlagsOkay() error {
+	c := 0
+	if resultCmdFlags.internalDebug {
+		c++
+	}
+	if resultCmdFlags.netVerbose {
+		c++
+	}
+	if resultCmdFlags.verbose {
+		c++
+	}
+	if c > 1 {
+		return ErrIncompatibleFlags
+	}
+	return nil
+}
+
+// RenderPerCmdline performs the table rendering requested from the
+// command-line, or iteration in non-table form, printing the item passed.
+// This is otherwise per-command boilerplate.
+func (rs *ResultSet) RenderPerCmdline(defaultPI printItem) {
+	if resultCmdFlags.verbose {
+		rs.PrintTables()
+	} else if resultCmdFlags.netVerbose {
+		rs.SelectFieldsNet()
+		rs.PrintTables()
+	} else if resultCmdFlags.internalDebug {
+		rs.SelectFieldsDebug()
+		rs.PrintTables()
+	} else {
+		rs.PrintPlain(defaultPI)
+	}
+}

commit 84264c5973626e6cd0ebf97dacf67d421b26f86f

Restore --oneline broken in 8b30966c

commands/named/named.go
@@ -6,6 +6,7 @@ package named
 
 import (
 	"errors"
+	"fmt"
 	"strings"
 
 	"github.com/atotto/clipboard"
@@ -41,6 +42,10 @@ var namedCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		if err := resultset.FlagsOkay(); err != nil {
 			root.Errorf("%s", err)
+			return
+		}
+		if resultset.CmdVerbose() && flags.oneline {
+			root.Errorf("can't use --oneline and a verbose table")
 			return
 		}
 		srcs := sources.NewFast()
@@ -107,7 +112,11 @@ var namedCmd = &cobra.Command{
 			}
 		}
 
-		results.RenderPerCmdline(resultset.PRINT_RUNE)
+		if flags.oneline {
+			fmt.Println(results.StringPlain(resultset.PRINT_RUNE))
+		} else {
+			results.RenderPerCmdline(resultset.PRINT_RUNE)
+		}
 	},
 }
 