d18c5b55041cc5de799cf10b416c051643d217a0
Phil Pennock <phil+git@pennock-tech.com>
2022-11-15 00:42:12 -0500

    Post-release -devel version

--- repo_version.go
+++ repo_version.go
@@ -8,7 +8,7 @@ import "github.com/philpennock/character/commands/version"
 
 // This should be "latest release + 0.0.1 with -devel suffix"
 // OR if at the point where we're tagging, then that explicit tag.
-const HARDCODED_VERSION = "v0.6.2"
+const HARDCODED_VERSION = "v0.6.3-devel"
 
 func init() {
 	if version.VersionString == "" {
