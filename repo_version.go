d04397cacab82f02bc037e9484e37fc03c792e1c
Phil Pennock <phil+git@pennock-tech.com>
2025-09-09 16:15:13 -0400

    Set repo version 0.9.3

--- repo_version.go
+++ repo_version.go
@@ -8,7 +8,7 @@ import "github.com/philpennock/character/commands/version"
 
 // This should be "latest release + 0.0.1 with -devel suffix"
 // OR if at the point where we're tagging, then that explicit tag.
-const HARDCODED_VERSION = "v0.9.3-devel"
+const HARDCODED_VERSION = "v0.9.3"
 
 func init() {
 	if version.VersionString == "" {
