e8cce5cd4b89504af64a05740e5b49c001e48ddd
Phil Pennock <phil+git@pennock-tech.com>
2026-03-18 06:56:27 -0400

    bump version to post-release (incr)-devel

--- repo_version.go
+++ repo_version.go
@@ -8,7 +8,7 @@ import "github.com/philpennock/character/commands/version"
 
 // This should be "latest release + 0.0.1 with -dev suffix"
 // OR if at the point where we're tagging, then that explicit tag.
-const HARDCODED_VERSION = "v0.10.3"
+const HARDCODED_VERSION = "v0.10.4-dev"
 
 func init() {
 	if version.VersionString == "" {
