96a947f286e060cd37102ae3e1470902f80dc636
Phil Pennock <phil+git@pennock-tech.com>
2026-03-24 23:04:01 -0400

    chore: bump hard-coded version to v0.10.6

--- repo_version.go
+++ repo_version.go
@@ -8,7 +8,7 @@ import "github.com/philpennock/character/commands/version"
 
 // This should be "latest release + 0.0.1 with -dev suffix"
 // OR if at the point where we're tagging, then that explicit tag.
-const HARDCODED_VERSION = "v0.10.6-dev"
+const HARDCODED_VERSION = "v0.10.6"
 
 func init() {
 	if version.VersionString == "" {
